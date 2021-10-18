99d616161c06677d97ed5fe0a615abeb23ba7d04
Kyoungkyu Park <kkpark@clouflake.com>
2021-10-18 17:07:07 +0900

    Initial Commit

---
+++ test/spaceone.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as Spaceone from '../lib/spaceone-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new Spaceone.SpaceoneStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
